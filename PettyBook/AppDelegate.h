4c77402ebd3b8bbf7bde42ede7a25b97a94c59dd
Carmelo Ruymán Quintana Santana <canariseven@gmail.com>
2015-03-28 16:46:33 +0000

    Initial Commit

---
+++ PettyBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PettyBook
+//
+//  Created by Carmelo Ruymán Quintana Santana on 28/3/15.
+//  Copyright (c) 2015 Carmelo Ruymán Quintana Santana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PettyBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PettyBook
+//
+//  Created by Carmelo Ruymán Quintana Santana on 28/3/15.
+//  Copyright (c) 2015 Carmelo Ruymán Quintana Santana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
