7b79b64b32f4aa5dcc981a8603cae12640ac23cf
Carmelo Ruymán Quintana Santana <canariseven@gmail.com>
2015-04-01 19:34:32 +0100

    Refactored JSON download method to asynchronously

--- PettyBook/AGTLoadingDataViewController.m
+++ PettyBook/AGTLoadingDataViewController.m
@@ -31,48 +31,83 @@
     [self dowloadLibrary];
 }
 -(void)dowloadLibrary{
-    NSError *error;
-    error = [self loadJSONAndCreateLibrary];
+    [self loadJSONAndCreateLibrary];
     
-    [self.activityIndicator stopAnimating];
-    self.activityIndicator.hidden = YES;
     
-    if (error == nil) {
-        [self presentViewControllersWithLibrary:self.lib];
-    }else{
-        NSLog(@"Error con el JSON: \n%@",error.localizedDescription);
-    }
-  
+    
+    
+    
 }
 
-- (NSError *)loadJSONAndCreateLibrary{
+- (void)loadJSONAndCreateLibrary{
     NSURL * url =  [NSURL URLWithString:URL_LIBRARY_JSON];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    NSURLResponse *response = [[NSURLResponse alloc] init];
-    
-    NSError *error;
-    NSData *data= [NSURLConnection sendSynchronousRequest:request
-                                        returningResponse:&response error:&error];
-    
-
-    if (data != nil) {
-        NSArray *JSONObjects = [NSJSONSerialization JSONObjectWithData:data
-                                                               options:kNilOptions
-                                                                 error:&error];
-        if (JSONObjects != nil) {
-            NSMutableArray * arr = [[NSMutableArray alloc]init];
-            for (NSDictionary *dict in JSONObjects){
-                AGTBook *book = [[AGTBook alloc] initWithDictionary:dict];
-                [arr addObject:book];
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    sessionConfiguration.HTTPAdditionalHeaders = @{@"Accept"    : @"application/json"};
+    // Inicialización de la sesión
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration];
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url];
+    // Tarea de gestión de datos
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse *HTTPResponse = (NSHTTPURLResponse *)response;
+        if (HTTPResponse.statusCode == 200) {
+            if (!error) {
+                if (data != nil) {
+                    NSError *err;
+                    NSArray *JSONObjects = [NSJSONSerialization JSONObjectWithData:data
+                                                                           options:kNilOptions
+                                                                             error:&err];
+                    if (JSONObjects != nil) {
+                        NSMutableArray * arr = [[NSMutableArray alloc]init];
+                        for (NSDictionary *dict in JSONObjects){
+                            AGTBook *book = [[AGTBook alloc] initWithDictionary:dict];
+                            [arr addObject:book];
+                        }
+                        self.books = arr;
+                        AGTLibrary * library = [[AGTLibrary alloc] initWithBooks:self.books];
+                        self.lib = library;
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            [self.activityIndicator stopAnimating];
+                            self.activityIndicator.hidden = YES;
+                            [self presentViewControllersWithLibrary:self.lib];
+                        });
+                        
+                    }else{
+                        NSLog(@"Fallo JSON");
+                    }
+                }
+            }else{
+                NSLog(@"Fallo al recibir el json");
             }
-            self.books = arr;
-            AGTLibrary * library = [[AGTLibrary alloc] initWithBooks:self.books];
-            self.lib = library;
-            return nil;
+        }else{
+            NSLog(@"Fallo servidor");
         }
-        return error;
-    }
-    return error;
+    }];
+    [dataTask resume];
+    
+    // Modo sincrono.
+    //    NSError *error;
+    //    NSData *data= [NSURLConnection sendSynchronousRequest:request
+    //                                        returningResponse:&response error:&error];
+    //
+    //
+    //    if (data != nil) {
+    //        NSArray *JSONObjects = [NSJSONSerialization JSONObjectWithData:data
+    //                                                               options:kNilOptions
+    //                                                                 error:&error];
+    //        if (JSONObjects != nil) {
+    //            NSMutableArray * arr = [[NSMutableArray alloc]init];
+    //            for (NSDictionary *dict in JSONObjects){
+    //                AGTBook *book = [[AGTBook alloc] initWithDictionary:dict];
+    //                [arr addObject:book];
+    //            }
+    //            self.books = arr;
+    //            AGTLibrary * library = [[AGTLibrary alloc] initWithBooks:self.books];
+    //            self.lib = library;
+    //            return nil;
+    //        }
+    //        return error;
+    //    }
+    //    return error;
 }
 
 -(void)presentViewControllersWithLibrary:(AGTLibrary *)library{
